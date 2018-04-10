bcc36908af49d8b1db4ada584245419b8757ea49
nykim <ppphlox@gmail.com>
2018-04-10 16:19:24 +0900

    중간저장

--- js/ui.js
+++ js/ui.js
@@ -1,105 +1,100 @@
 $(function(){
+  window.sr = ScrollReveal();
 
-  var $item = $(".item-fade");
+  sr.reveal('.fade-sec1-tit',{
+                origin: 'left',
+                opacity: 0,
+                scale: 1,
+                duration: 900,
+              }
+            );
+  sr.reveal('.fade-sec1-desc', {
+                delay: 950,
+                opacity: 0,
+                scale: 1,
+                duration: 720,
+              },450
+            );
+  sr.reveal('.fade-sec2-pic', {
+                distance: '350px',
+                opacity: 0,
+                scale: 1,
+                duration: 700,
+              }
+            );
+  sr.reveal('.fade-sec2-tit', {
+                delay: 660,
+                opacity: 0,
+                scale: 1,
+                duration: 700,
+              }
+            );
+  sr.reveal('.fade-sec2-desc', {
+                delay: 1320,
+                opacity: 0,
+                scale: 1,
+                duration: 700,
+              }
+            );
+  sr.reveal('.fade-sec3-list', {
+              distance: '120px',
+              opacity: 0,
+              scale: 1,
+              duration: 720,
+            }
+          );
 
-  function getOffsetVal(elem){
-    var itemOffset = (window.scrollY >= ( elem.offsetTop - window.innerHeight + 300 ) ) && (window.scrollY <= (elem.offsetTop + elem.offsetHeight - 250) );	
-		return itemOffset;
+
+  /*** randomly show ***/
+  var goalNum = [];
+
+  //난수 생성
+  function generateRandom(max){
+    return Math.floor(Math.random()*max);
   }
 
-	$item.each(function(e) {
-    console.log("아이템 발견 !!!!")
-
-
-
-		if(getOffsetVal($(this))){
-      $(this).addClass("HELLO");
-    }
-	});
-  
-});
-
-
-$(function(){
-
-  var scrollY = window.scrollY;
-  var scrollTop = $(window).scrollTop(); 
-  console.log("scrollY=", scrollY);
-  console.log("scrollTop=", scrollTop);
-
-});
-
-
-$(function(){
-
-var rafId = null;
-var delay = 200;
-var lTime = 0;
-
-function scroll() {
-  var scrollTop = $(window).scrollTop(); 
-  var height = $(window).height()
-  var visibleTop = scrollTop + height;
-  $('.reveal').each(function() {
-    var $t = $(this);
-    if ($t.hasClass('reveal_visible')) { return; }
-    var top = $t.offset().top;
-    if (top <= visibleTop) {
-      if (top + $t.height() < scrollTop) {
-        $t.removeClass('reveal_pending').addClass('reveal_visible');
-      } else {
-        $t.addClass('reveal_pending');
-        if (!rafId) requestAnimationFrame(reveal);  
+  //연속적인 난수 생성
+  function generateGoalNumber(){
+    for(var i=0; i<8; i++){
+      goalNum[i] = generateRandom(8);
+      
+      //겹치는 숫자 제거
+      for (var j = 0; j<i; j++) {
+        if(goalNum[i] == goalNum[j]){
+          i = i - 1; 
+          break;
+        }
       }
     }
-  });
-}
-function reveal() {
-  rafId = null;
-  var now = performance.now();
-  
-  if (now - lTime > delay) {
-    lTime = now;
-    var $ts = $('.reveal_pending');
-    $($ts.get(0)).removeClass('reveal_pending').addClass('reveal_visible');  
   }
+
+  generateGoalNumber();
+  //console.log(goalNum);
   
+  for (var i=0; i<=7; i++){
+    sr.reveal('.fade-sec3-num'+goalNum[i], {
+        distance: '120px',
+        opacity: 0,
+        scale: 1,
+        //container: '#team',
+        //viewFactor: 0.9,
+        viewport: document.getElementById('team'),
+        duration: 630,
+        delay: (i*200),
+        easing: 'cubic-bezier(0.77, 0, 0.175, 1)',
+      }
+    );
+  }
+
   
-  if ($('.reveal_pending').length >= 1) rafId = requestAnimationFrame(reveal);
-}
+  sr.reveal('.fade-sec4-list', {
+      opacity: 0,
+      scale: 1,
+      duration: 720,
+      viewFactor: 0.9,
+    }, 780
+  );
 
-$(scroll);
-$(window).scroll(scroll);
-$(window).click(function() {
-  $('.reveal').removeClass('reveal_visible').removeClass('reveal_pending');
-  lTime = performance.now() + 500;
-  var top = $(window).scrollTop();
-  $(window).scrollTop(top === 0 ? 1 : top-1);
-});
+
 
 });
-
-
-
-// var $item =	$('.item-fade'),
-// $item_link = $('.item-link');
-// function offsetValue(i){
-// var item_offset = (window.scrollY >= ( i.offsetTop - window.innerHeight + 300 ) ) && (window.scrollY <= (i.offsetTop + i.offsetHeight - 250) );	
-// return item_offset;
-// };
-// $item_link.forEach(function(link){
-// link.addEventListener('click',function(evt){
-//   evt.preventDefault();
-//   var scroll_position = $(this.getAttribute('href')).offsetTop - 100;
-//   window.scroll(0,scroll_position);
-// })	
-// });
-// $item.forEach(function(e) {
-// var item_id = e.getAttribute('id');
-// if(offsetValue(e)){
-//   e.classList.add('act');
-//   $('a[href*=' + item_id + ']').classList.add('act');
-// }else{
-//   $('a[href*=' + item_id + ']').classList.remove('act');
-// }
-// });
